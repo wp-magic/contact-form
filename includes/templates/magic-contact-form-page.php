8631191cb2f923f70987a2a2b5a21473542b4739
Jascha Ehrenreich <jascha@jaeh.at>
2018-07-12 21:07:16 +0200

    whitespace

--- includes/templates/magic-contact-form-page.php
+++ includes/templates/magic-contact-form-page.php
@@ -11,8 +11,8 @@ $context = Timber::get_context();
 
 $context['post'] = new TimberPost();
 
-if (!empty( $_POST ) ) {
-  require_once 'requests/send-messsage.php';
+if ( !empty( $_POST ) ) {
+  require_once 'requests/send-message.php';
   $context = magic_cf_send_message( $context );
 }
 
@@ -22,7 +22,7 @@ $context['form'] = array(
   'nonce' => wp_create_nonce( MAGIC_CONTACT_FORM_SEND_ACTION ),
 );
 
-if ($user = wp_get_current_user() ) {
+if ( $user = wp_get_current_user() ) {
   $context['query']['username'] = $user->display_name;
   $context['query']['email'] = $user->user_email;
 }
